42453b126e17155f74d237a2ab85e71cdce8755b
galang-ctrl <galangdatabase@gmail.com>
2024-10-05 16:16:08 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from '/src/views/LoginScreen.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+createApp(App).mount('#app')
---
+++ src/views/LoginScreen.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="text-center">Sistem Informasi Pemakaian abc</h3>
+            </div>
+            <div class="card-body">
+              <form @submit.prevent="handleLogin">
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    v-model="email"
+                    required
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    v-model="password"
+                    required
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary btn-block">Login</button>
+                <div v-if="errorMessage" class="alert alert-danger mt-3">{{ errorMessage }}</div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    </script>
+  
+  <style scoped>
+  /* You can add custom styles here if needed */
+  </style>
+  
